1044211e0628a7269c9d99c409458d9f37147b27
danielbuva <daniel.valdecantos@gmail.com>
2023-06-30 16:05:17 -0700

    refactor: add more providers & use create root

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import { ColorProvider } from "./components/ColorProvider.jsx";
+import { ModalProvider } from "./components/Modal/index.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-import { ModalProvider, Modal } from "./context/Modal";
-import configureStore from "./store";
 import * as sessionActions from "./store/session";
-import App from "./App";
+import configureStore from "./store";
+
+import App from "./App.jsx";
 
 import "./index.css";
 
@@ -17,16 +19,16 @@ if (process.env.NODE_ENV !== "production") {
   window.sessionActions = sessionActions;
 }
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ModalProvider>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-          <Modal />
-        </BrowserRouter>
-      </Provider>
-    </ModalProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Provider store={store}>
+      <BrowserRouter>
+        <ColorProvider>
+          <ModalProvider>
+            <App />
+          </ModalProvider>
+        </ColorProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
