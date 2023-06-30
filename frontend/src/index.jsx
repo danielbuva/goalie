91b97a6a1489696279bbb788cae3078a6406c632
danielbuva <daniel.valdecantos@gmail.com>
2023-06-30 15:16:57 -0700

    feat: change js files to jsx

---
+++ frontend/src/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+import { ModalProvider, Modal } from "./context/Modal";
+import configureStore from "./store";
+import * as sessionActions from "./store/session";
+import App from "./App";
+
+import "./index.css";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
