8fe67e6666b64e523b761d8364651c2077672419
Melody Yoo <melodyyoo72@gmail.com>
2023-06-30 13:45:43 -0700

    fix: change 'react-app' to 'frontend'

--- backend/__init__.py
+++ backend/__init__.py
@@ -10,7 +10,7 @@ from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
 
-app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 
 # Setup login manager
 login = LoginManager(app)
