5a6c9d1a650d5511c7c68c09872af9e5dafd4411
danielbuva <daniel.valdecantos@gmail.com>
2023-06-30 16:07:40 -0700

    feat: add layout component

---
+++ frontend/src/components/Layout/index.jsx
@@ -0,0 +1,75 @@
+import "./Layout.css";
+import GoalieLogo from "../GoalieLogo";
+import NavButtons from "../NavButtons";
+import Title from "../Title";
+import UserMenu from "../UserMenu";
+// import { ShowModalButton } from "../Modal";
+
+function Layout({ children }) {
+  return (
+    <div id="layout">
+      <div id="left">
+        <div id="content-left-container">
+          <div id="content-left">
+            <div id="logo-div">
+              <GoalieLogo />
+            </div>
+            <div>
+              <NavButtons />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="main">
+        <Title />
+        {children}
+      </div>
+      <div id="right">
+        <div id="content-right-container">
+          <div id="content-right">
+            <UserMenu />
+            <Credits />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Credits() {
+  return (
+    <div id="credits">
+      <p>dev team:</p>
+      <a
+        href="https://github.com/melodyyoo"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Melody Yoo
+      </a>
+      <a
+        href="https://github.com/huanglallen"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Allen Huang
+      </a>
+      <a
+        href="https://github.com/ihavenoide"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        James Hernandez
+      </a>
+      <a
+        href="https://github.com/danielbuva"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Daniel Valdecantos
+      </a>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ frontend/src/components/icons/Ellipsis.jsx
@@ -0,0 +1,35 @@
+function Ellipsis() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="5"
+      viewBox="0 0 16 5"
+      fill="none"
+    >
+      <circle
+        className="icon"
+        cx="2.46048"
+        cy="2.48514"
+        r="1.87796"
+        fill="black"
+      />
+      <circle
+        className="icon"
+        cx="8.23148"
+        cy="2.50125"
+        r="1.87796"
+        fill="black"
+      />
+      <circle
+        className="icon"
+        cx="13.7608"
+        cy="2.48514"
+        r="1.87796"
+        fill="black"
+      />
+    </svg>
+  );
+}
+
+export default Ellipsis;
