a34f777c6c707fbdb5219e5889f6ec776b8d3fd8
danielbuva <daniel.valdecantos@gmail.com>
2023-07-01 18:43:26 -0700

    refactor: user route to return based on username

--- backend/api/user_routes.py
+++ backend/api/user_routes.py
@@ -2,24 +2,19 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from backend.models import User
 
-user_routes = Blueprint('users', __name__)
+user_routes = Blueprint("users", __name__)
 
 
-@user_routes.route('/')
+@user_routes.route("/")
 @login_required
 def users():
     """
     Query for all users and returns them in a list of user dictionaries
     """
     users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
+    return {"users": [user.to_dict() for user in users]}
 
 
-@user_routes.route('/<int:id>')
-@login_required
+@user_routes.route("/<string:id>")
 def user(id):
-    """
-    Query for a user by id and returns that user in a dictionary
-    """
-    user = User.query.get(id)
-    return user.to_dict()
+    return User.query.filter(User.username == id).first().to_dict()
