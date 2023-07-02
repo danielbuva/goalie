a72e572dd535f261d0cd9b966a9bc2dd38331c45
danielbuva <daniel.valdecantos@gmail.com>
2023-07-01 21:00:03 -0700

    feat: add hover attribute to avatar

--- frontend/src/components/Avatar/index.jsx
+++ frontend/src/components/Avatar/index.jsx
@@ -1,8 +1,12 @@
 import pic from "./placeholder-goalie.jpeg";
 import "./index.css";
 
-export default function Avatar({ boxSize = "45px", src = pic }) {
-  return (
+export default function Avatar({
+  boxSize = "45px",
+  hover = true,
+  src = pic,
+}) {
+  return hover ? (
     <div className="overlay" style={{ width: boxSize, height: boxSize }}>
       <img
         className="profile-pic"
@@ -11,5 +15,12 @@ export default function Avatar({ boxSize = "45px", src = pic }) {
         style={{ width: boxSize, height: boxSize }}
       />
     </div>
+  ) : (
+    <img
+      className="profile-pic"
+      src={src}
+      alt="profile-pic"
+      style={{ width: boxSize, height: boxSize }}
+    />
   );
 }
