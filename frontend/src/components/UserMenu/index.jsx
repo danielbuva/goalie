b9022e7ab3479ed76413ec1a43bc38f030a60400
danielbuva <daniel.valdecantos@gmail.com>
2023-07-01 17:07:04 -0700

    feat: add user info to user menu

--- frontend/src/components/UserMenu/index.jsx
+++ frontend/src/components/UserMenu/index.jsx
@@ -5,15 +5,31 @@ import SignupForm from "../SignupForm";
 import LoginForm from "../LoginForm";
 import Avatar from "../Avatar";
 import "./Usermenu.css";
+import useSessionUser from "../../hooks/useSessionUser";
 
 function UserMenu() {
   const { buttonRef, menuRef, toggleMenu, show, hideMenu } = useMenu();
   const { showModal } = useModal();
+  const currentUser = useSessionUser();
+
   return (
     <>
-      <div className="usermenu" onClick={toggleMenu} ref={buttonRef}>
-        <Avatar />
-        <Ellipsis />
+      <div
+        className="user-menu"
+        onClick={toggleMenu}
+        ref={buttonRef}
+        style={{ width: currentUser ? "250px" : "45px" }}
+      >
+        <div className="user">
+          <Avatar />
+          {currentUser && (
+            <div className="user-info">
+              <p className="user-menu-name">daniel valdecantos</p>
+              <p className="user-menu-username">@danibuva</p>
+            </div>
+          )}
+        </div>
+        {currentUser && <Ellipsis />}
       </div>
       <Menu isOpen={show} menuRef={menuRef}>
         <MenuItem
