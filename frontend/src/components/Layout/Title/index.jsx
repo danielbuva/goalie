4fd36c58d0448e41d39dffb672e2d3fbffad9a56
danielbuva <daniel.valdecantos@gmail.com>
2023-07-01 18:44:13 -0700

    feat: change profile titles to first last names

--- frontend/src/components/Layout/Title/index.jsx
+++ frontend/src/components/Layout/Title/index.jsx
@@ -1,9 +1,12 @@
 import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Title() {
   const { pathname } = useLocation();
+  const user = useSelector((state) => state.users.user);
 
   let title = "";
+
   if (pathname.includes("/home")) {
     title = "Home";
   } else if (pathname.includes("/challenges")) {
@@ -11,7 +14,7 @@ function Title() {
   } else if (pathname.includes("/communities")) {
     title = "Communities";
   } else {
-    title = pathname.slice(1);
+    title = user.firstName + " " + user.lastName;
   }
 
   return <h2 id="title">{title}</h2>;
