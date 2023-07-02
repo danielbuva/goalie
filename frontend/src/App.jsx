de10e6ffc258f71e2cea0dbe708226c0466fec31
danielbuva <daniel.valdecantos@gmail.com>
2023-07-01 17:11:12 -0700

    fix: clear warnings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
 import Layout from "./components/Layout";
-import AllGoals from './components/Goals/AllGoals'
-import UserGoals from "./components/Goals/UserGoals";
-import ProfilePage from './components/ProfilePage'
+import AllGoals from "./components/Goals/AllGoals";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/home" element={<AllGoals/>}/>
+        <Route path="/home" element={<AllGoals />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
-        <Route path="/:userId" element={<ProfilePage/>} />
+        <Route path="/:userId" element={<ProfilePage />} />
       </Routes>
     </Layout>
   );
