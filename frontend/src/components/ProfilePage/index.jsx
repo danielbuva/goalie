64ffe8b97029194d251b4c0b2d48e5e4e04de5f3
danielbuva <daniel.valdecantos@gmail.com>
2023-07-01 19:31:18 -0700

    fix: hide folow button on own profile page

--- frontend/src/components/ProfilePage/index.jsx
+++ frontend/src/components/ProfilePage/index.jsx
@@ -1,3 +1,4 @@
+import useSessionUser from "../../hooks/useSessionUser";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getUser } from "../../store/users";
@@ -9,6 +10,7 @@ import "./index.css";
 
 export default function ProfilePage() {
   const user = useSelector((state) => state.users.user);
+  const currentUser = useSessionUser();
   const dispatch = useDispatch();
   const { userId } = useParams();
 
@@ -16,25 +18,25 @@ export default function ProfilePage() {
     dispatch(getUser(userId));
   }, [dispatch, userId]);
 
-  console.log(user);
-
   if (!user) return null;
 
+  const isOwnProfile = currentUser.username === user.username;
+
   return (
-    <div className="profile-page">
-      <div className="profile-page-banner"></div>
-      <div className="profile-page-top-half">
+    <div className="profile">
+      <div className="profile-banner"></div>
+      <div className="profile-header">
         <div>
-          <div className="profile-page-bio-button-holder">
-            <div className="profile-page-bio-follows">
+          <div className="profile-bio-button-holder">
+            <div className="profile-bio-follows">
               <Avatar boxSize="136px" />
-              <h2>{user.firstName}</h2>
+              <h2 style={{ paddingTop: "16px" }}>{user.firstName}</h2>
               <p>
                 <span>@{user.username}</span>
               </p>
 
-              <p className="profile-page-bio">{user.bio}</p>
-              <div className="profile-page-followings-holder">
+              <p className="profile-bio">{user.bio}</p>
+              <div className="profile-followings-holder">
                 <p>
                   0<span> Following </span>
                 </p>
@@ -43,18 +45,11 @@ export default function ProfilePage() {
                 </p>
               </div>
             </div>
-            <div>
-              <button>Follow</button>
-            </div>
+            {!isOwnProfile && <button>Follow</button>}
           </div>
         </div>
-        {/* <Tabs>
-          <Tab text="Goals" />
-          <Tab text="Challenges" />
-          <Tab text="Acomplished" />
-        </Tabs> */}
       </div>
-      <div className="profile-page-bottom-half">
+      <div className="profile-bottom-half">
         <UserGoals />
       </div>
     </div>
