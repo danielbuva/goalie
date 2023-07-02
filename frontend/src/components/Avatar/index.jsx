be71fca4219adc993b4a781eb068f82a42c8f218
danielbuva <daniel.valdecantos@gmail.com>
2023-07-01 19:09:02 -0700

    feat: add overlay on hover to avatars

--- frontend/src/components/Avatar/index.jsx
+++ frontend/src/components/Avatar/index.jsx
@@ -3,11 +3,13 @@ import "./index.css";
 
 export default function Avatar({ boxSize = "45px", src = pic }) {
   return (
-    <img
-      className="profile-pic"
-      src={src}
-      alt="profile-pic"
-      style={{ width: boxSize, height: boxSize }}
-    />
+    <div className="overlay" style={{ width: boxSize, height: boxSize }}>
+      <img
+        className="profile-pic"
+        src={src}
+        alt="profile-pic"
+        style={{ width: boxSize, height: boxSize }}
+      />
+    </div>
   );
 }
