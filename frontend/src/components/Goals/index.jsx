1fe30c8e58777cc47f2c6b7406de1847a30b0f7a
danielbuva <daniel.valdecantos@gmail.com>
2023-07-01 19:02:44 -0700

    chore: format code

--- frontend/src/components/Goals/index.jsx
+++ frontend/src/components/Goals/index.jsx
@@ -1,21 +1,20 @@
 import Post from "../Post";
 
-export default function Goals({goals}) {
-
+export default function Goals({ goals }) {
   if (!goals || goals.length < 1) return null;
 
   return (
     <div>
       {goals.map((goal) => (
-          <Post
-            key={goal.id}
-            title={goal.title}
-            doit={goal.doit}
-            createdAt={goal.createdAt}
-            body={goal.body}
-            user={goal.user}
-          />
-        ))}
+        <Post
+          key={goal.id}
+          title={goal.title}
+          doit={goal.doit}
+          createdAt={goal.createdAt}
+          body={goal.body}
+          user={goal.user}
+        />
+      ))}
     </div>
   );
 }
