0b686f6fdc0bf36b8662f38cfb26f43cb5e37156
Melody Yoo <melodyyoo72@gmail.com>
2023-07-03 09:27:40 -0700

    refactor: only fill icon if own profile

--- frontend/src/components/Layout/NavButtons/icons/Profile.jsx
+++ frontend/src/components/Layout/NavButtons/icons/Profile.jsx
@@ -1,11 +1,10 @@
 import { useLocation } from "react-router-dom";
+import useSessionUser from "../../../../hooks/useSessionUser";
 
 function Profile() {
   const { pathname } = useLocation();
-  const isOnProfile =
-    !pathname.includes("challenges") &&
-    !pathname.includes("communities") &&
-    !pathname.includes("home");
+  const currentUser = useSessionUser()
+  const isOnProfile = pathname.includes(currentUser.id)
 
   const d = isOnProfile
     ? "M2.651 17 Z M 3.137 11.44 C 4.627 9.85 6.648 9 9 9 C 11.352 9 13.373 9.85 14.863 11.44 C 16.34 13.02 17.229 15.24 17.495 17.9 L 17.605 19 H 0.395 L 0.505 17.9 C 0.771 15.24 1.66 13.02 3.137 11.44 Z Z M 5 4 C 5 1.79 6.791 0 9 0 C 11.209 0 13 1.79 13 4 C 13 6.21 11.209 8 9 8 C 6.791 8 5 6.21 5 4 Z"
