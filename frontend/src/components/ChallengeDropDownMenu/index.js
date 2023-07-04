3b7ffa85b36e96dec940c923f750f053175c7ad9
danielbuva <daniel.valdecantos@gmail.com>
2023-07-04 14:28:08 -0700

    fix: menu position

--- frontend/src/components/ChallengeDropDownMenu/index.js
+++ frontend/src/components/ChallengeDropDownMenu/index.js
@@ -1,16 +1,14 @@
 import useSessionUser from "../../hooks/useSessionUser";
 import useChallenge from "../../hooks/useChallenge";
-import { useDispatch } from "react-redux";
 import { Menu, MenuItem, useMenu } from "../Menu";
-import { useModal } from "../../hooks/useModal";
-import { JoinChallenge, LeaveChallenge } from "../../store/challenges";
+import { useDispatch } from "react-redux";
 import Ellipsis from "../icons/Ellipsis";
 
 export default function ChallengeDropDownMenu({ challenge }) {
   const dispatch = useDispatch();
   const user = useSessionUser();
   const challenges = useChallenge();
-  const { buttonRef, menuRef, toggleMenu, show, hideMenu } = useMenu();
+  const { buttonRef, menuRef, toggleMenu, show } = useMenu();
 
   let isParticipant = challenge.allParticipants.find((participant) =>
     user ? participant.userId == user.id : false
@@ -18,24 +16,20 @@ export default function ChallengeDropDownMenu({ challenge }) {
 
   let isOwner = user ? challenge.creatorId == user.id : false;
 
-  let editClicker = () => {};
-
   return (
-    <div onClick={(e) => e.stopPropagation()}>
-      {/* {isOwner && <div>Edit Challenge</div>}
-      {isOwner && <div>Delete Challenge </div>}
-      {!isParticipant && <div>Join Challenge</div>}
-      {isParticipant && <div>Leave Challenge</div>} */}
-      <Ellipsis
-        buttonRef={buttonRef}
-        onClick={(e) => {
-          toggleMenu();
-        }}
-      />
+    <div
+      onClick={(e) => e.stopPropagation()}
+      style={{ position: "relative" }}
+    >
+      <Ellipsis buttonRef={buttonRef} onClick={toggleMenu} />
       <Menu isOpen={show} menuRef={menuRef} right>
         {isOwner && <MenuItem text="Edit" />}
         {isOwner && <MenuItem text="Delete" />}
-        {isParticipant ? <MenuItem text="Leave" /> : <MenuItem text="Join" />}
+        {isParticipant ? (
+          <MenuItem text="Leave" />
+        ) : (
+          <MenuItem text="Join" />
+        )}
       </Menu>
     </div>
   );
