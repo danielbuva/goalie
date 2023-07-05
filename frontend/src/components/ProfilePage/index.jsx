12a1c04e0a4e6a16049bffc5cf8acf8b3caa17be
danielbuva <daniel.valdecantos@gmail.com>
2023-07-04 22:54:25 -0700

    feat: add joined and styles

--- frontend/src/components/ProfilePage/index.jsx
+++ frontend/src/components/ProfilePage/index.jsx
@@ -1,16 +1,19 @@
-import {
-  Link,
-  useLocation,
-  useNavigate,
-  useParams,
-} from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import useSessionUser from "../../hooks/useSessionUser";
 import { useSelector, useDispatch } from "react-redux";
+import { useColorMode } from "../../hooks/useTheme";
 import { getUser } from "../../store/users";
 import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import Avatar from "../Avatar";
 import "./index.css";
+
+function getMonthYear(dateString) {
+  const date = new Date(dateString);
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+  return `${month} ${year}`;
+}
 
 export default function ProfilePage() {
   const user = useSelector((state) => state.users.user);
@@ -19,22 +22,30 @@ export default function ProfilePage() {
   const dispatch = useDispatch();
   const { userId } = useParams();
 
+  const col = useColorMode("#fff", "#15202B", "#000");
+
   useEffect(() => {
     dispatch(getUser(userId));
   }, [dispatch, userId]);
 
+  const tabColor = useColorMode("#0f1419", "#f7f9f9");
+
   if (!user) return null;
 
-  const tabPosition = pathname.includes("challenges")
+  const challengesTab = pathname.includes("challenges");
+  const accomplishedTab = pathname.includes("accomplished");
+  const communitiesTab = pathname.includes("communities");
+  const goalsTab = pathname === "/" + user.id;
+
+  const tabPosition = challengesTab
     ? 158
-    : pathname.includes("accomplished")
+    : accomplishedTab
     ? 312
-    : pathname.includes("communities")
+    : communitiesTab
     ? 474
     : 35;
 
   const isOwnProfile = currentUser?.id === user.id;
-
   return (
     <div className="profile">
       <div className="profile-banner"></div>
@@ -42,13 +53,17 @@ export default function ProfilePage() {
         <div>
           <div className="profile-bio-button-holder">
             <div className="profile-bio-follows">
-              <Avatar boxSize="136px" />
+              <Avatar
+                boxSize="136px"
+                border={`solid 4px ${col}`}
+                borderRadius="100%"
+              />
               <h2 style={{ paddingTop: "16px" }}>{user.name}</h2>
-              <p>
-                <span>@{user.id}</span>
-              </p>
-
+              <p id="profile-username">@{user.id}</p>
               <p className="profile-bio">{user.bio}</p>
+              <p id="joined-at">
+                <Calendar /> Joined {getMonthYear(user.createdAt)}
+              </p>
               <div className="profile-followings-holder">
                 <p>
                   0<span> Following </span>
@@ -63,15 +78,49 @@ export default function ProfilePage() {
         </div>
       </div>
       <div className="profile-tabs">
-        <Link to={`/${userId}`}>Goals</Link>
-        <Link to={`/${userId}/challenges`}>Challenges</Link>
-        <Link to={`/${userId}/accomplished`}>Accomplished</Link>
-        <Link to={`/${userId}/communities`}>Communities</Link>
+        <Link
+          to={`/${userId}`}
+          style={{ color: goalsTab ? tabColor : "#8b98a5" }}
+        >
+          Goals
+        </Link>
+        <Link
+          to={`/${userId}/challenges`}
+          style={{ color: challengesTab ? tabColor : "#8b98a5" }}
+        >
+          Challenges
+        </Link>
+        <Link
+          to={`/${userId}/accomplished`}
+          style={{ color: accomplishedTab ? tabColor : "#8b98a5" }}
+        >
+          Accomplished
+        </Link>
+        <Link
+          to={`/${userId}/communities`}
+          style={{ color: communitiesTab ? tabColor : "#8b98a5" }}
+        >
+          Communities
+        </Link>
         <div className="profile-indicator" style={{ left: tabPosition }} />
       </div>
       <div className="profile-bottom-half">
         <Outlet />
       </div>
     </div>
+  );
+}
+
+function Calendar() {
+  return (
+    <svg
+      width="18.75px"
+      height="18.75px"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      fill="#8b98a5"
+    >
+      <path d="M7 4V3h2v1h6V3h2v1h1.5C19.89 4 21 5.12 21 6.5v12c0 1.38-1.11 2.5-2.5 2.5h-13C4.12 21 3 19.88 3 18.5v-12C3 5.12 4.12 4 5.5 4H7zm0 2H5.5c-.27 0-.5.22-.5.5v12c0 .28.23.5.5.5h13c.28 0 .5-.22.5-.5v-12c0-.28-.22-.5-.5-.5H17v1h-2V6H9v1H7V6zm0 6h2v-2H7v2zm0 4h2v-2H7v2zm4-4h2v-2h-2v2zm0 4h2v-2h-2v2zm4-4h2v-2h-2v2z"></path>
+    </svg>
   );
 }
