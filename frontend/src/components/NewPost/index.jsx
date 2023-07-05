bb259ce34a84b3fc3027714b6d14e5c3ede6c40b
danielbuva <daniel.valdecantos@gmail.com>
2023-07-04 21:04:08 -0700

    feat: add frontend error handling to add goal

--- frontend/src/components/NewPost/index.jsx
+++ frontend/src/components/NewPost/index.jsx
@@ -9,9 +9,18 @@ function NewPost({ type = "goal", post, index }) {
   const [title, setTitle] = useState(post?.title ?? "");
   const [body, setBody] = useState(post?.body ?? "");
   const [errors, setErrors] = useState({});
+  const [show, setShow] = useState(false);
   const dispatch = useDispatch();
   const { closeModal } = useModal();
+
+  const titleHasError = title.length > 50;
+  const bodyHasError = body.length > 255;
+  const noBody = body.length === 0;
+
   const handleClick = async () => {
+    if (titleHasError || bodyHasError || noBody) {
+      return setShow(true);
+    }
     let res;
     if (post?.body) {
       res = await dispatch(
@@ -30,29 +39,62 @@ function NewPost({ type = "goal", post, index }) {
 
   const bodyPlaceholder =
     type === "goal" ? "Write a goal..." : "Challenge the world...";
+
+  const titleErrorStyle = titleHasError
+    ? { color: "#f4212e" }
+    : title.length >= 40
+    ? { color: "inherit" }
+    : { opacity: 0 };
+
+  const bodyErrorStyle = bodyHasError
+    ? { color: "#f4212e" }
+    : body.length >= 230
+    ? { color: "inherit" }
+    : { opacity: 0 };
+
   return (
     <div id="new-post">
       <div id="new-post-header">
         <Avatar hover={false} />
         <div id="new-post-content">
-          <input
-            id="new-post-title"
-            placeholder="Title"
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-          />
-          <textarea
-            id="new-post-body"
-            placeholder={bodyPlaceholder}
-            onChange={(e) => setBody(e.target.value)}
-            value={body}
-          />
+          <div className="new-post-b">
+            <input
+              id="title-input"
+              placeholder="Title"
+              onChange={(e) => {
+                setShow(false);
+                setTitle(e.target.value);
+              }}
+              value={title}
+            />
+            <p style={titleErrorStyle}>{title.length}/50</p>
+          </div>
+          <div className="new-post-b">
+            <textarea
+              id="new-post-body"
+              placeholder={bodyPlaceholder}
+              onChange={(e) => {
+                setShow(false);
+                setBody(e.target.value);
+              }}
+              value={body}
+            />
+            <p style={bodyErrorStyle}>{body.length}/255</p>
+          </div>
         </div>
       </div>
 
       <div id="divider" />
-
       <div id="new-post-footer">
+        <div id="new-post-errors">
+          <p style={{ opacity: show && noBody ? 1 : 0 }}>Enter a goal!</p>
+          <p style={{ opacity: show && titleHasError ? 1 : 0 }}>
+            Title too long!
+          </p>
+          <p style={{ opacity: show && bodyHasError ? 1 : 0 }}>
+            Body too long!
+          </p>
+        </div>
         <button id="new-post-submit" onClick={handleClick}>
           Goal +
         </button>
