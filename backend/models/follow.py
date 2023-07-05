be1c027a87f9449dd3fb9f5d485f6f15fa9621ea
huanglallen <huanglallen@gmail.com>
2023-07-05 09:14:48 -0700

    Follows: model bug fixed

--- backend/models/follow.py
+++ backend/models/follow.py
@@ -1,22 +1,22 @@
-# from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA
+from .user import User
+
+class Follow(db.Model):
+    __tablename__ = "follows"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    following_id = db.Column(db.String(40), db.ForeignKey("users.id"), primary_key=True)
+    follower_id = db.Column(db.String(40), db.ForeignKey("users.id"), primary_key=True)
+    createdAt = db.Column(db.DateTime, nullable=False)
+    UpdatedAt = db.Column(db.DateTime, nullable=False)
 
 
-# class Follow(db.Model):
-#     __tablename__ = "follows"
-
-#     if environment == "production":
-#         __table_args__ = {"schema": SCHEMA}
-
-#     following_id = db.Column(db.Integer, primary_key=True)
-#     follower_id = db.Column(db.Integer, primary_key=True)
-#     createdAt = db.Column(db.DateTime, nullable=False)
-#     UpdatedAt = db.Column(db.DateTime, nullable=False)
-
-
-#     followers = db.relationship(
-#     "User",
-#     secondary=__tablename__,
-#     primaryjoin=(__tablename__.c.follower_id == id),
-#     secondaryjoin=(__tablename__.c.following_id == id),
-#     backref="following",
-# )
+    followers = db.relationship(
+    "User",
+    secondary=__tablename__,
+    primaryjoin=(__tablename__.c.follower_id == id),
+    secondaryjoin=(__tablename__.c.following_id == id),
+    backref="following",
+)
