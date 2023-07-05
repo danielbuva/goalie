7084f517d0be102d97b7045dacee75961e02e85e
danielbuva <daniel.valdecantos@gmail.com>
2023-07-04 23:00:25 -0700

    refactor: use hook for src in not found page

--- frontend/src/components/PageNotFound/index.jsx
+++ frontend/src/components/PageNotFound/index.jsx
@@ -1,21 +1,26 @@
-import { useTheme } from "../../hooks/useTheme";
-import src1 from "./404-white.png"
-import src2 from "./404-black.png"
-import src3 from "./404-dim.png"
+import { useColorMode } from "../../hooks/useTheme";
 import { useNavigate } from "react-router-dom";
-import "./index.css"
+
+import src1 from "./images/404-white.png";
+import src2 from "./images/404-dim.png";
+import src3 from "./images/404-black.png";
+
+import "./index.css";
 
 export default function PageNotFound() {
-    const {theme} = useTheme();
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const src = theme==='light' ? src1 : theme ==='black' ? src2 : src3
+  const src = useColorMode(src1, src2, src3);
 
   return (
     <div className="page-not-found">
       <h1>Oh no! The page you're looking for doesn't exist.</h1>
-      <button onClick={()=>navigate('/home')}>Take me back home</button>
-      <img className="page-not-found-img" src={src}/>
+      <button onClick={() => navigate("/home")}>Take me back home</button>
+      <img
+        className="page-not-found-img"
+        src={src}
+        alt="404 Goaly Goalie"
+      />
     </div>
   );
 }
