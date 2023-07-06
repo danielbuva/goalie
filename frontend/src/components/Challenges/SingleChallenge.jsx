013c4163d4106abc5b118d38017192d62d0b1b30
Melody Yoo <melodyyoo72@gmail.com>
2023-07-06 09:38:23 -0700

    refactor: dynamically render avatar for challenges

--- frontend/src/components/Challenges/SingleChallenge.jsx
+++ frontend/src/components/Challenges/SingleChallenge.jsx
@@ -1,18 +1,20 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import ChallengeDropDownMenu from "../ChallengeDropDownMenu";
 import Avatar from "../Avatar";
 import "./SingleChallenge.css";
+import { useSelector } from "react-redux";
 
 export default function SingleChallenge({ challenge }) {
   const history = useNavigate();
   let singleChallengeClicker = () => {
     history(`/challenges/${challenge.id}`);
   };
+  const user = useSelector(state=> state.users.user)
+  const {pathname: path} = useLocation();
+
   return (
     <div className="challengeWrapper" onClick={singleChallengeClicker}>
-      <div>
-        <Avatar />
-      </div>
+      {!path.includes(user?.id) &&  <Avatar/>}
       <div className="singelChallenge-text">
         <div className="singelChallenge-text-first">{challenge.title}</div>
         <div className="singelChallenge-text-second">
