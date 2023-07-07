710317fa32d2b64b3fbc0f2640e4569754796249
danielbuva <daniel.valdecantos@gmail.com>
2023-07-06 23:52:22 -0700

    feat: add check mark to completed goal posts

--- frontend/src/components/Post/index.jsx
+++ frontend/src/components/Post/index.jsx
@@ -1,11 +1,12 @@
-import { Link, useParams } from "react-router-dom";
-import Avatar from "../Avatar";
-import "./Post.css";
-import Options from "./Options";
-import { timeSince } from "../../utils";
-import { useDispatch } from "react-redux";
-import { addDoit, removeDoit } from "../../store/goals";
 import useSessionUser from "../../hooks/useSessionUser";
+import { Link, useParams } from "react-router-dom";
+import { addDoit, removeDoit } from "../../store/goals";
+import { useDispatch } from "react-redux";
+import { timeSince } from "../../utils";
+import Options from "./Options";
+import Avatar from "../Avatar";
+
+import "./Post.css";
 
 export function Post({
   title,
@@ -28,7 +29,11 @@ export function Post({
         <Options post={{ title, body, id, status }} index={index} />
       </div>
 
-      <p className="post-body">{body}</p>
+      <div className="post-main">
+        <div className="post-body">{body}</div>
+        {status && <AccomplishedGoalMark />}
+      </div>
+
       <div className="post-footer">
         <Doit doit={doit} id={id} />
         <p className="post-timestamp">{timeSince(createdAt)}</p>
@@ -43,7 +48,7 @@ function User({ user }) {
   return (
     <div className="post-header">
       <Link to={profileLink}>
-        <Avatar src={user?.image}/>
+        <Avatar src={user?.image} />
       </Link>
       {user && (
         <>
@@ -81,6 +86,21 @@ function Doit({ doit, id }) {
       <span>doit</span>
       {doit.length}
     </div>
+  );
+}
+
+function AccomplishedGoalMark() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="1.25em"
+      viewBox="0 0 512 512"
+    >
+      <path
+        className="theme-icon"
+        d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"
+      />
+    </svg>
   );
 }
 
