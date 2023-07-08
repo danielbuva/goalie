7f82b0a6eff52251d7dfc87cce47cffe365e7d95
Melody Yoo <melodyyoo72@gmail.com>
2023-07-08 01:46:29 -0700

    style: rough rough draft of edit profile page

--- frontend/src/components/EditProfile/index.jsx
+++ frontend/src/components/EditProfile/index.jsx
@@ -1,14 +1,14 @@
 import { useDispatch } from "react-redux";
-import { useColorMode } from "../../hooks/useTheme";
-import Avatar from "../Avatar";
+// import { useColorMode } from "../../hooks/useTheme";
 import "./index.css";
+import "../Avatar/index.css";
 import { useRef, useState } from "react";
 import useSessionUser from "../../hooks/useSessionUser";
 import { updateUser } from "../../store/users";
 import { useModal } from "../../hooks/useModal";
 
 export default function EditProfile() {
-  const col = useColorMode("#fff", "#15202B", "#000");
+  // const col = useColorMode("#fff", "#15202B", "#000");
   const dispatch = useDispatch();
   const currentUser = useSessionUser();
   const [name, setName] = useState(currentUser.name);
@@ -31,60 +31,52 @@ export default function EditProfile() {
 
     closeModal();
   };
-
+  // style={{ height: "100px", zIndex: "-2", marginTop: "30" }}
   return (
     <form className="edit-profile-div" encType="multipart/form-data" onSubmit={handleClick}>
-      <div className="profile-banner">
+      <div className="edit-profile-banner">
         <div className="edit-photo-div">
-          <svg
-            viewBox="0 0 24 24"
-            width="26"
-            height="26"
-            className="camera-icon"
+          <i
+            className="fa-solid fa-camera"
             onClick={() => {
-              if (bannerInputRef.current) bannerInputRef.current.click();
+              if (bannerInputRef.current) {
+                bannerInputRef.current.click();
+              }
             }}
-          >
-            <g>
-              <path
-                className="icon"
-                d="M9.697 3H11v2h-.697l-3 2H5c-.276 0-.5.224-.5.5v11c0 .276.224.5.5.5h14c.276 0 .5-.224.5-.5V10h2v8.5c0 1.381-1.119 2.5-2.5 2.5H5c-1.381 0-2.5-1.119-2.5-2.5v-11C2.5 6.119 3.619 5 5 5h1.697l3-2zM12 10.5c-1.105 0-2 .895-2 2s.895 2 2 2 2-.895 2-2-.895-2-2-2zm-4 2c0-2.209 1.791-4 4-4s4 1.791 4 4-1.791 4-4 4-4-1.791-4-4zM17 2c0 1.657-1.343 3-3 3v1c1.657 0 3 1.343 3 3h1c0-1.657 1.343-3 3-3V5c-1.657 0-3-1.343-3-3h-1z"
-              ></path>
-            </g>
-          </svg>
+          ></i>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setBanner(e.target.files[0])}
+            style={{ visibility: "hidden" }}
+            ref={bannerInputRef}
+          />
         </div>
+      </div>
+      <div className="edit-profile-avatar">
         <input
           type="file"
           accept="image/*"
-          onChange={(e) => setBanner(e.target.files[0])}
+          onChange={(e) => setImage(e.target.files[0])}
           style={{ visibility: "hidden" }}
-          ref={bannerInputRef}
+          ref={imageInputRef}
         />
-      </div>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={(e) => setImage(e.target.files[0])}
-        style={{ visibility: "hidden" }}
-        ref={imageInputRef}
-      />
-      <Avatar boxSize="136px" border={`solid 4px ${col}`} borderRadius="100%" hover={false}>
         <div
-          className="edit-photo-div"
+          className="edit-avatar-div"
           onClick={(e) => {
             imageInputRef.current?.click();
           }}
         >
-          <svg viewBox="0 0 24 24" width="26" height="26" className="camera-icon">
-            <g>
-              <path
-                className="icon"
-                d="M9.697 3H11v2h-.697l-3 2H5c-.276 0-.5.224-.5.5v11c0 .276.224.5.5.5h14c.276 0 .5-.224.5-.5V10h2v8.5c0 1.381-1.119 2.5-2.5 2.5H5c-1.381 0-2.5-1.119-2.5-2.5v-11C2.5 6.119 3.619 5 5 5h1.697l3-2zM12 10.5c-1.105 0-2 .895-2 2s.895 2 2 2 2-.895 2-2-.895-2-2-2zm-4 2c0-2.209 1.791-4 4-4s4 1.791 4 4-1.791 4-4 4-4-1.791-4-4zM17 2c0 1.657-1.343 3-3 3v1c1.657 0 3 1.343 3 3h1c0-1.657 1.343-3 3-3V5c-1.657 0-3-1.343-3-3h-1z"
-              ></path>
-            </g>
-          </svg>
+          <i
+            className="fa-solid fa-camera"
+            onClick={() => {
+              if (imageInputRef.current) {
+                imageInputRef.current.click();
+              }
+            }}
+          ></i>
         </div>
-      </Avatar>
+      </div>
       <input
         type="text"
         className="edit-name"
