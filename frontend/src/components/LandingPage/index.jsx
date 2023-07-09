f0ed5c08069231b50ef5b18de6ea1eda2b6a8a22
danielbuva <daniel.valdecantos@gmail.com>
2023-07-08 19:55:39 -0700

    feat: add working nav to landing page

--- frontend/src/components/LandingPage/index.jsx
+++ frontend/src/components/LandingPage/index.jsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom";
-import GoalieLogo from "../GoalieLogo";
+import { useModal } from "../../hooks/useModal";
 import ChallengesExample from "./challenges.png";
+import GoalieLogo from "../GoalieLogo";
+import SignupForm from "../SignupForm";
+import LoginForm from "../LoginForm";
+
 import "./index.css";
 
 export default function LandingPage() {
+  const { showModal } = useModal();
   return (
     <div style={{ height: "100%" }}>
-      <nav>
-        <GoalieLogo/>
-        <Link>Login</Link>
-        <Link>Sign Up</Link>
+      <nav id="lp-nav">
+        <GoalieLogo />
+        <div id="lp-auth">
+          <p onClick={() => showModal(<LoginForm />)}>Login</p>
+          <p onClick={() => showModal(<SignupForm />)}>Sign Up</p>
+        </div>
       </nav>
       <div>
         <div className="lp lp-welcome">
@@ -20,10 +26,11 @@ export default function LandingPage() {
           <h1>Support your friends' goals with doit.</h1>
         </div>
         <div className="lp lp-challenges">
-          <h1>
-            Create and participate in challenges with one another.
-          </h1>
-          <img alt="Example of Challenges Feature" src={ChallengesExample}></img>
+          <h1>Create and participate in challenges with one another.</h1>
+          <img
+            alt="Example of Challenges Feature"
+            src={ChallengesExample}
+          ></img>
         </div>
         <div>
           <h2 className="lp">Communities coming soon...</h2>
